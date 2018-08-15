9b74c971df9e90d769f7f5855a4bc9a770925d20
Christopher Zell <Zelldon91@gmail.com>
2018-08-15 15:45:34 +0200

    add deploying

--- test-runner/src/main/java/io/zeebe/workbench/impl/TestRunner.java
+++ test-runner/src/main/java/io/zeebe/workbench/impl/TestRunner.java
@@ -1,14 +1,38 @@
 package io.zeebe.workbench.impl;
 
+import io.zeebe.broker.Broker;
+import io.zeebe.broker.system.configuration.BrokerCfg;
+import io.zeebe.gateway.ZeebeClient;
+import io.zeebe.gateway.api.clients.TopicClient;
+import io.zeebe.util.sched.clock.ActorClock;
 import io.zeebe.workbench.*;
 
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class TestRunner implements Runner {
 
+  private final String tempFolder;
+  private final Broker broker;
+
+  private final ZeebeClient zeebeClient = ZeebeClient.newClient();
+  private final TopicClient topicClient;
+
   private final HashMap<String, WorkflowResource> deployedResources = new HashMap<>();
+
+  public TestRunner() {
+    try {
+      tempFolder = Files.createTempDirectory("zeebe").toAbsolutePath().normalize().toString();
+      BrokerCfg cfg = new BrokerCfg();
+      cfg.setBootstrap(1);
+      broker = new Broker(cfg, tempFolder, (ActorClock) null);
+    } catch (Exception ex) {
+      throw new IllegalStateException("Broker start does not work.");
+    }
+    topicClient = zeebeClient.topicClient();
+  }
 
   @Override
   public List<TestResult> run(List<WorkflowResource> resources, List<TestCase> cases) {
@@ -17,21 +41,22 @@ public class TestRunner implements Runner {
     return runTests(cases);
   }
 
-  private void deploy(List<WorkflowResource> resources)
-  {
-    for (WorkflowResource resource : resources)
-    {
+  private void deploy(List<WorkflowResource> resources) {
+    for (WorkflowResource resource : resources) {
       deployedResources.put(resource.getName(), resource);
 
-      // TODO deploy on broker
+      topicClient
+          .workflowClient()
+          .newDeployCommand()
+          .addResourceBytes(resource.getResource(), resource.getName())
+          .send()
+          .join();
     }
   }
 
-  private List<TestResult> runTests(List<TestCase> cases)
-  {
+  private List<TestResult> runTests(List<TestCase> cases) {
     final List<TestResult> results = new ArrayList<>();
-    for (TestCase testCase : cases)
-    {
+    for (TestCase testCase : cases) {
       runTest(testCase);
     }
     return results;
@@ -52,14 +77,11 @@ public class TestRunner implements Runner {
 
     // TODO verify current state
     final List<Verification> verifications = testCase.getVerifications();
-    for (Verification verification : verifications)
-    {
+    for (Verification verification : verifications) {
       // TODO verify expected state
       // TODO add failed verification on failed verification
     }
 
     return result;
   }
-
-
 }
