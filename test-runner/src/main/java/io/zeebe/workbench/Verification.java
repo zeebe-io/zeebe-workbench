8f2ede445c88dea0a4f0304196e6fe7b5ae04801
Christopher Zell <Zelldon91@gmail.com>
2018-08-17 09:58:32 +0200

    fix verify epexted payload

--- test-runner/src/main/java/io/zeebe/workbench/Verification.java
+++ test-runner/src/main/java/io/zeebe/workbench/Verification.java
@@ -2,6 +2,7 @@ package io.zeebe.workbench;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class Verification {
 
@@ -11,13 +12,19 @@ public class Verification {
 
   private final String activityId;
 
+  public Verification(String expectedIntent, String expectedPayload, String activityId) {
+    this.expectedIntent = expectedIntent;
+    this.expectedPayload = expectedPayload;
+    this.activityId = activityId;
+  }
+
   @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
   public Verification(
       @JsonProperty("expectedIntent") String expectedIntent,
-      @JsonProperty("expectedPayload") String expectedPayload,
+      @JsonProperty("expectedPayload") JsonNode expectedPayload,
       @JsonProperty("activityId") String activityId) {
     this.expectedIntent = expectedIntent;
-    this.expectedPayload = expectedPayload;
+    this.expectedPayload = expectedPayload.toString();
     this.activityId = activityId;
   }
 
